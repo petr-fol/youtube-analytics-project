8f6ed874fac6bff0413dfedf3338528908eec7f4
petr-fol <144835428+petr-fol@users.noreply.github.com>
2023-10-21 17:16:13 +0600

    release homework 4

---
+++ src/video.py
@@ -0,0 +1,48 @@
+import json
+
+from googleapiclient.discovery import build
+import os
+
+
+class Video:
+
+    def __init__(self, video_id):
+        self.__video_id = video_id
+        self.video = self.get_service().videos().list(part='snippet,statistics', id=self.__video_id).execute()
+        self.title = self.video['items'][0]['snippet']['title']
+        self.url = f"https://www.youtube.com/watch?v={self.__video_id}"
+        self.view_count = self.video['items'][0]['statistics']['viewCount']
+        self.like_count = self.video['items'][0]['statistics']['likeCount']
+
+    @classmethod
+    def get_service(cls):
+        api_key = os.environ.get("youtube_API")
+        youtube = build('youtube', 'v3', developerKey=api_key)
+        return youtube
+
+    def print_info(self) -> None:
+        """Выводит в консоль информацию о канале."""
+        print(json.dumps(self.video, indent=2, ensure_ascii=False))
+
+    def __str__(self):
+        return self.title
+
+
+class PLVideo:
+    def __init__(self, video_id, playlist_id):
+        self.__video_id = video_id
+        self.playlist_id = playlist_id
+        self.video = self.get_service().videos().list(part='snippet,statistics', id=self.__video_id).execute()
+        self.title = self.video['items'][0]['snippet']['title']
+        self.url = f"https://www.youtube.com/watch?v={self.__video_id}"
+        self.view_count = self.video['items'][0]['statistics']['viewCount']
+        self.like_count = self.video['items'][0]['statistics']['likeCount']
+
+    @classmethod
+    def get_service(cls):
+        api_key = os.environ.get("youtube_API")
+        youtube = build('youtube', 'v3', developerKey=api_key)
+        return youtube
+
+    def __str__(self):
+        return self.title
